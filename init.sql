5b258e88f8e96968e8c9735e8d1157880c217d67
Quincy Hsieh <quincy.tw@gmail.com>
2017-04-29 17:55:17 +0200

    Create table aujourdhui

--- init.sql
+++ init.sql
@@ -1,5 +1,10 @@
-CREATE DATABASE projet_theatre;;
+CREATE DATABASE projet_theatre;
 
-\connect projet_theatre;;
+\connect projet_theatre;
+
+CREATE TABLE IF NOT EXISTS aujourdhui (id integer, time timestamp, PRIMARY KEY(id));
+
+INSERT INTO aujourdhui VALUES (0, to_timestamp('13:30 14/04/2017', 'HH24:MI DD/MM/YYYY'));
+
 
 
