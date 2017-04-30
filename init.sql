62e687a11104ff16e13559b248470c00fca98605
Fan <fanyizhe3015@gmail.com>
2017-04-30 17:06:50 +0200

    serial

--- init.sql
+++ init.sql
@@ -6,19 +6,18 @@ CREATE DATABASE projet_theatre;
 ----------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS Aujourdhui (
-	id integer, 
-	time timestamp, 
-	PRIMARY KEY(id)
+	id SERIAL PRIMARY KEY, 
+	time timestamp
 );
 
-INSERT INTO Aujourdhui (id, time) VALUES 
-(0, to_timestamp('13:30 14/04/2017', 'HH24:MI DD/MM/YYYY')),
-(1, '2017-04-30 13:30');
+INSERT INTO Aujourdhui (time) VALUES 
+(to_timestamp('13:30 14/04/2017', 'HH24:MI DD/MM/YYYY')),
+('2017-04-30 13:30');
 
 ----------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS Spectacle (
-	IdSpectacle integer PRIMARY KEY,
+	IdSpectacle SERIAL PRIMARY KEY,
 	Nom varchar(20) NOT NULL,
 	Type integer NOT NULL
 		CHECK (Type IN (0,1)),/* 0: cree, 1:achete */
@@ -30,15 +29,15 @@ CREATE TABLE IF NOT EXISTS Spectacle (
 		CHECK (TarifNormal >= 0)
 );
 
-INSERT INTO Spectacle (IdSpectacle, Nom, Type, Places, TarifNormal, TarifReduit) VALUES
-(0, 'Carmen', 0, 85, 204.45, 144.45),
-(1, 'La Dame Blanche', 0, 55, 60.05, 26.0),
-(2, 'La Peur', 1, 35, 30.9, 18.30);
+INSERT INTO Spectacle (Nom, Type, Places, TarifNormal, TarifReduit) VALUES
+('Carmen', 0, 85, 204.45, 144.45),
+('La Dame Blanche', 0, 55, 60.05, 26.0),
+('La Peur', 1, 35, 30.9, 18.30);
 
 ----------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS Organisme (
-        IdOrganisme integer PRIMARY KEY,
+        IdOrganisme SERIAL PRIMARY KEY,
         Nom varchar(20) NOT NULL,
         Type varchar(20)
 );
@@ -60,7 +59,7 @@ CREATE TABLE IF NOT EXISTS Subventions (
 ----------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS Cout_Spectacle (
-        IdCout integer PRIMARY KEY,
+        IdCout SERIAL PRIMARY KEY,
         IdSpectacle integer NOT NULL references Spectacle, 
         /* Trigger insert before selon type de spectacle si il a type <achete>, on depense que une seule fois */
         Date_Depenser date NOT NULL,
@@ -68,19 +67,19 @@ CREATE TABLE IF NOT EXISTS Cout_Spectacle (
         	CHECK (Montant > 0)
 );
 
-INSERT INTO Cout_Spectacle (IdCout, IdSpectacle, Date_Depenser, Montant) VALUES
-(0, 0, '2014-01-07', 30.01),
-(1, 0, '2014-01-10', 10.50),
-(2, 1, '2015-10-10', 100.10),
-(3, 1, '2015-10-30', 25.09),
-(4, 1, '2015-12-10', 1.98),
-(5, 1, '2016-01-10', 45),
-(6, 2, '2016-12-23', 1000.10);
+INSERT INTO Cout_Spectacle (IdSpectacle, Date_Depenser, Montant) VALUES
+(1, '2014-01-07', 30.01),
+(1, '2014-01-10', 10.50),
+(2, '2015-10-10', 100.10),
+(2, '2015-10-30', 25.09),
+(2, '2015-12-10', 1.98),
+(2, '2016-01-10', 45),
+(3, '2016-12-23', 1000.10);
 
 ----------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS Repre_Interne (
-        IdRepresentation integer PRIMARY KEY,
+        IdRepresentation SERIAL PRIMARY KEY,
         IdSpectacle integer NOT NULL references Spectacle,
         Date_Sortir date NOT NULL,
         Politique integer NOT NULL
@@ -93,5 +92,4 @@ CREATE TABLE IF NOT EXISTS Repre_Interne (
     pour cle etranger on pense a utiliser 
     ON DELETE CASCADE 
     ON DELETE RESTRICT
-    question serial
 */
