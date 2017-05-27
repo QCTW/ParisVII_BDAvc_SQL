99b395e230a9e87146b4b831330901ab6f4be931
Fan <fanyizhe3015@gmail.com>
2017-05-27 21:51:20 +0200

    functions

--- drop_functions.sql
+++ drop_functions.sql
@@ -1,8 +1,8 @@
 -- Drop functions
-DROP FUNCTION reserver();
-DROP FUNCTION calc_numbre_place_dans_billet();
-DROP FUNCTION calc_numbre_place_dans_reserv();
-DROP FUNCTION get_seat_sold_percentage();
-DROP FUNCTION get_seat_full_percentage();
-DROP FUNCTION get_current_ticket_price();
+DROP FUNCTION reserver(integer,integer);
+DROP FUNCTION calc_numbre_place_dans_billet(integer);
+DROP FUNCTION calc_numbre_place_dans_reserv(integer);
+DROP FUNCTION get_seat_sold_percentage(integer);
+DROP FUNCTION get_seat_full_percentage(integer);
+DROP FUNCTION get_current_ticket_price(integer,integer);
 DROP FUNCTION pay_reservation(integer,integer,integer);
